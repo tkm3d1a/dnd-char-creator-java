45b0c108d85bc1bbd15a133ebcadc921a9747fbc
Tim Klimpel <tim.klimpel@gmail.com>
2023-02-20 07:41:22 -0500

    Initial Commit

---
+++ src/main/java/com/tkm3d1a/dndcharactercreator/DndcharactercreatorApplication.java
@@ -0,0 +1,13 @@
+package com.tkm3d1a.dndcharactercreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndcharactercreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DndcharactercreatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tkm3d1a/dndcharactercreator/DndcharactercreatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tkm3d1a.dndcharactercreator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndcharactercreatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
